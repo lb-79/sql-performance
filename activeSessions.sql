cbb4811603bb88ff3d565c17eaab415ea0a7b043
lb79 <125643374+lb-79@users.noreply.github.com>
2023-03-03 17:44:13 +0100

    Add files via upload

---
+++ activeSessions.sql
@@ -0,0 +1,22 @@
+--Active Sessions (if sp_whoisactive can't be used)
+SELECT
+	s.session_id,
+	r.start_time,
+	s.host_name,
+	s.login_name,
+	i.event_info,
+	r.status,
+	s.program_name,
+	r.writes,
+	r.reads,
+	r.logical_reads,
+	r.blocking_session_id,
+	r.wait_type,
+	r.wait_time,
+	r.wait_resource
+FROM sys.dm_exec_requests r
+JOIN sys.dm_exec_sessions s
+ON s.session_id = r.session_id
+CROSS APPLY sys.dm_exec_input_buffer (s.session_id,r.request_id) as i
+WHERE s.session_id != @@SPID
+AND s.is_user_process = 1
